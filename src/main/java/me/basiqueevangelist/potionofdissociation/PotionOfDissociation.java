f97125680f84fee6b2f04071fd9ebdc0cfb98333
Basique Evangelist <basiqueevangelist@yandex.ru>
2022-11-28 00:55:20 +0300

    add long potion

--- src/main/java/me/basiqueevangelist/potionofdissociation/PotionOfDissociation.java
+++ src/main/java/me/basiqueevangelist/potionofdissociation/PotionOfDissociation.java
@@ -12,13 +12,16 @@ import net.minecraft.util.registry.Registry;
 public class PotionOfDissociation implements ModInitializer {
     public static final DissociationStatusEffect EFFECT = new DissociationStatusEffect();
     public static final Potion POTION = new Potion(new StatusEffectInstance(EFFECT, 20 * 60 * 2));
+    public static final Potion LONG_POTION = new Potion("dissociation", new StatusEffectInstance(EFFECT, 20 * 60 * 5));
 
     @Override
     public void onInitialize() {
         Registry.register(Registry.STATUS_EFFECT, id("dissociation"), EFFECT);
         Registry.register(Registry.POTION, id("dissociation"), POTION);
+        Registry.register(Registry.POTION, id("long_dissociation"), LONG_POTION);
         BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, Items.RED_MUSHROOM, POTION);
         BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, Items.BROWN_MUSHROOM, POTION);
+        BrewingRecipeRegistry.registerPotionRecipe(POTION, Items.REDSTONE, LONG_POTION);
     }
 
     private Identifier id(String path) {
