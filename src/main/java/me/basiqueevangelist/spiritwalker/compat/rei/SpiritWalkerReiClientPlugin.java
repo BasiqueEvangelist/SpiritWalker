ac863cb1c88a828f002a45528d4c943a666531de
Basique <basiqueevangelist@yandex.ru>
2023-06-19 16:26:29 +0300

    make rei plugin a bit more resilient

--- src/main/java/me/basiqueevangelist/spiritwalker/compat/rei/SpiritWalkerReiClientPlugin.java
+++ src/main/java/me/basiqueevangelist/spiritwalker/compat/rei/SpiritWalkerReiClientPlugin.java
@@ -17,7 +17,9 @@ public class SpiritWalkerReiClientPlugin implements REIClientPlugin {
             if (!SpiritWalker.CONFIG.disableSplashAndLingering()) return EventResult.pass();
             if (!category.getCategoryIdentifier().equals(BuiltinPlugin.BREWING)) return EventResult.pass();
 
-            BrewingRecipe recipe = (BrewingRecipe) registry.getDisplayOrigin(display);
+            if (!(registry.getDisplayOrigin(display) instanceof BrewingRecipe recipe))
+                return EventResult.pass();
+
             if (SpiritWalker.POTIONS.contains(PotionUtil.getPotion(recipe.output))
              && recipe.output.getItem() != Items.POTION)
                 return EventResult.interruptFalse();
